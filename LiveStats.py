f2658c7866adb76b138ef63b1b9d6f44e0c727ac
Wouter van der Zwan <mail@wvdzwan.nl>
2013-05-31 15:14:51 +0200

    Added csv output to the LiveStats script
    The people from WebSolarLog (http://www.websolarlog.com/) asked if it was possible to add a csv output so they can use this script. So I added a --csv (or --wsl) parameter to the LiveStats script. If that parameter is present the human readable output will be replaced with a csv output.

--- LiveStats.py
+++ LiveStats.py
@@ -5,6 +5,14 @@ import sys, os
 import socket               # Needed for talking to inverter
 import ConfigParser
 
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("--csv", "--wsl", help="Output in csv format", action="store_true", default=False )
+
+(options, args) = parser.parse_args()
+
+
 # Load the setting
 mydir = os.path.dirname(os.path.abspath(__file__))
 
@@ -52,15 +60,29 @@ while 1:
 s.close()
 
 msg = InverterMsg.InverterMsg(data)  # This is where the magic happens ;)
-print "ID: {0}".format(msg.getID())
 
-print "E Today: {0:>5}   Total: {1:<5}".format(msg.getEToday(), msg.getETotal())
-print "H Total: {0:>5}   Temp:  {1:<5}".format(msg.getHTotal(), msg.getTemp())
-
-print "PV1   V: {0:>5}   I: {1:>4}".format(msg.getVPV(1), msg.getIPV(1))
-print "PV2   V: {0:>5}   I: {1:>4}".format(msg.getVPV(2), msg.getIPV(2))
-print "PV3   V: {0:>5}   I: {1:>4}".format(msg.getVPV(3), msg.getIPV(3))
-
-print "L1    P: {0:>5}   V: {1:>5}   I: {2:>4}   F: {3:>5}".format(msg.getPAC(1), msg.getVAC(1), msg.getIAC(1), msg.getFAC(1)) 
-print "L2    P: {0:>5}   V: {1:>5}   I: {2:>4}   F: {3:>5}".format(msg.getPAC(2), msg.getVAC(2), msg.getIAC(2), msg.getFAC(2)) 
-print "L3    P: {0:>5}   V: {1:>5}   I: {2:>4}   F: {3:>5}".format(msg.getPAC(3), msg.getVAC(3), msg.getIAC(3), msg.getFAC(3)) 
+if not options.csv:
+    print "ID: {0}".format(msg.getID())
+    
+    print "E Today: {0:>5}   Total: {1:<5}".format(msg.getEToday(), msg.getETotal())
+    print "H Total: {0:>5}   Temp:  {1:<5}".format(msg.getHTotal(), msg.getTemp())
+    
+    print "PV1   V: {0:>5}   I: {1:>4}".format(msg.getVPV(1), msg.getIPV(1))
+    print "PV2   V: {0:>5}   I: {1:>4}".format(msg.getVPV(2), msg.getIPV(2))
+    print "PV3   V: {0:>5}   I: {1:>4}".format(msg.getVPV(3), msg.getIPV(3))
+    
+    print "L1    P: {0:>5}   V: {1:>5}   I: {2:>4}   F: {3:>5}".format(msg.getPAC(1), msg.getVAC(1), msg.getIAC(1), msg.getFAC(1)) 
+    print "L2    P: {0:>5}   V: {1:>5}   I: {2:>4}   F: {3:>5}".format(msg.getPAC(2), msg.getVAC(2), msg.getIAC(2), msg.getFAC(2)) 
+    print "L3    P: {0:>5}   V: {1:>5}   I: {2:>4}   F: {3:>5}".format(msg.getPAC(3), msg.getVAC(3), msg.getIAC(3), msg.getFAC(3)) 
+else:
+    print "Id,Temp,VPV1,VPV2,VPV3,IPV1,IPV2,IPV3,IAC1,IAC2,IAC3,VAC1,VAC2,VAC3,FAC1,PAC1,FAC2,PAC2,FAC3,PAC3,ETODAY,ETOTAL,HTOTAL"
+    print "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21},{22}".format(msg.getID(),
+        msg.getTemp(), 
+        msg.getVPV(1), msg.getVPV(2), msg.getVPV(3), 
+        msg.getIPV(1), msg.getIPV(2), msg.getIPV(3),
+        msg.getIAC(1), msg.getIAC(2), msg.getIAC(3),
+        msg.getVAC(1), msg.getVAC(2), msg.getVAC(3),
+        msg.getFAC(1), msg.getPAC(1),
+        msg.getFAC(2), msg.getPAC(2),
+        msg.getFAC(3), msg.getPAC(3),
+        msg.getEToday(), msg.getETotal(), msg.getHTotal())   
