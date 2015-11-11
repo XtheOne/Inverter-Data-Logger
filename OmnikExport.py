39c6ed1e5a9e61c022f0ebb52967f61cb350756d
Wouter van der Zwan <mail@wvdzwan.nl>
2015-11-12 00:17:16 +0100

    Improved logger

--- OmnikExport.py
+++ OmnikExport.py
@@ -6,11 +6,12 @@ import socket               # Needed for talking to inverter
 import datetime             # Used for timestamp
 import sys
 import logging
+import logging.config
 import ConfigParser, os
 
 from PluginLoader import Plugin
 
-# For PVoutput 
+# For PVoutput
 import urllib, urllib2
 
 # Load the setting
@@ -29,18 +30,29 @@ pvout_enabled   = config.getboolean('pvout','pvout_enabled')
 pvout_apikey    = config.get('pvout','pvout_apikey')
 pvout_sysid     = config.get('pvout','pvout_sysid')
 
-log_enabled     = config.getboolean('log','log_enabled')
-log_filename    = mydir + '/' + config.get('log','log_filename')
-
-
 server_address = ((ip, port))
 
+# Build logger
+log_levels = dict(debug=10, info=20, warning=30, error=40, critical=50)
+log_dict = dict(version=1,
+                formatters={
+                    'f': {'format': '%(asctime)s %(levelname)s %(message)s'}
+                },
+                handlers={
+                    'none': {'class': 'logging.NullHandler'},
+                    'console': {'class': 'logging.StreamHandler',
+                                'formatter': 'f'},
+                    'file': {'class': 'logging.FileHandler',
+                             'filename': mydir + '/' + config.get('log', 'filename'),
+                             'formatter': 'f'},
+                },
+                loggers={'OmnikLogger': {
+                        'handlers': config.get('log', 'type').split(),
+                        'level': log_levels[config.get('log', 'level')]}
+                })
+
+logging.config.dictConfig(log_dict)
 logger = logging.getLogger('OmnikLogger')
-hdlr = logging.FileHandler(log_filename)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr) 
-logger.setLevel(logging.DEBUG)
 
 # Load output plugins
 # Prepare path for plugin loading
@@ -57,8 +69,7 @@ for plugin_name in config.get('general', 'enabled_plugins').split(','):
 for res in socket.getaddrinfo(ip, port, socket.AF_INET , socket.SOCK_STREAM):
     af, socktype, proto, canonname, sa = res
     try:
-        if log_enabled:
-            logger.info('connecting to %s port %s' % server_address)
+        logger.info('connecting to %s port %s' % server_address)
         s = socket.socket(af, socktype, proto)
         s.settimeout(10)
     except socket.error as msg:
@@ -73,8 +84,7 @@ for res in socket.getaddrinfo(ip, port, socket.AF_INET , socket.SOCK_STREAM):
     break
     
 if s is None:
-    if log_enabled:
-        logger.error('could not open socket')
+    logger.error('could not open socket')
     sys.exit(1)
     
 s.sendall(InverterMsg.generate_string(wifi_serial))
@@ -84,8 +94,7 @@ s.close()
 msg = InverterMsg.InverterMsg(data)  # This is where the magic happens ;)
 now = datetime.datetime.now()
 
-if log_enabled:
-    logger.info("ID: {0}".format(msg.getID())) 
+logger.info("ID: {0}".format(msg.getID()))
 
 for plugin in Plugin.plugins:
     logger.debug('Run plugin' + plugin.__class__.__name__)
@@ -93,8 +102,7 @@ for plugin in Plugin.plugins:
 
 
 if pvout_enabled and (now.minute % 5) == 0:
-    if log_enabled:
-        logger.info('Uploading to PVoutput')
+    logger.info('Uploading to PVoutput')
     url = "http://pvoutput.org/service/r2/addstatus.jsp"
     
     if use_temp:
@@ -123,7 +131,6 @@ if pvout_enabled and (now.minute % 5) == 0:
     
     request_object = urllib2.Request(url + '?' + get_data_encoded)      # Create request object
     response = urllib2.urlopen(request_object)                          # Make the request and store the response
-    
-    if log_enabled:
-        logger.info(response.read())                                               # Show the response
+
+    logger.info(response.read())                                               # Show the response
     
